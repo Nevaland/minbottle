cb9fb851f603e23f4588cf903783017560487f80
Nevaland <deanh0716@gmail.com>
2020-11-07 03:32:48 +0900

    Initial commit

---
+++ src/main/java/nevaland/minbottle/MinbottleApplication.java
@@ -0,0 +1,13 @@
+package nevaland.minbottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinbottleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinbottleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nevaland/minbottle/MinbottleApplicationTests.java
@@ -0,0 +1,13 @@
+package nevaland.minbottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinbottleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
