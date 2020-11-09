d00159c1a5ee69fa526ee932bc6c5985aaa0298a
Nevaland <deanh0716@gmail.com>
2020-11-09 19:35:53 +0900

    Add letter form

---
+++ src/main/java/nevaland/minbottle/controller/form/LetterForm.java
@@ -0,0 +1,13 @@
+package nevaland.minbottle.controller.form;
+
+public class LetterForm {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
