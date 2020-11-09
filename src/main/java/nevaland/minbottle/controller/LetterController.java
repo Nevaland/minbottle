b78e487ec14bbb0798027b902f9ed47671506934
Nevaland <deanh0716@gmail.com>
2020-11-09 19:36:46 +0900

    Implement index, letter controller

---
+++ src/main/java/nevaland/minbottle/controller/LetterController.java
@@ -0,0 +1,49 @@
+package nevaland.minbottle.controller;
+
+import nevaland.minbottle.controller.form.LetterForm;
+import nevaland.minbottle.domain.Letter;
+import nevaland.minbottle.service.LetterService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LetterController {
+
+    private final LetterService letterService;
+
+    public LetterController(LetterService letterService) {
+        this.letterService = letterService;
+    }
+
+    @GetMapping("/letter")
+    public String letter() {
+        return "letter/letter";
+    }
+
+    @GetMapping("/letter/new")
+    public String writeLetterForm() {
+        return "letter/writeLetterForm";
+    }
+
+    @PostMapping("/letter/new")
+    public String writeLetter(LetterForm form) {
+        Letter letter = new Letter();
+        letter.setContent(form.getContent());
+
+        letterService.write(letter);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/letter/edit")
+    public String editLetterForm() {
+        return "letter/editLetterForm";
+    }
+
+    @GetMapping("/letter/delete")
+    public String deleteLetterForm() {
+        return "letter/deleteLetterForm";
+    }
+
+}
