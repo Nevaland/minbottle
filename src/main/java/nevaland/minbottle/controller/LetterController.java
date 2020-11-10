3f2b8e2c9b273880b90d413558d0e6def1544179
Nevaland <deanh0716@gmail.com>
2020-11-10 13:18:08 +0900

    Add next letter button

--- src/main/java/nevaland/minbottle/controller/LetterController.java
+++ src/main/java/nevaland/minbottle/controller/LetterController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -24,6 +25,12 @@ public class LetterController {
         Letter letter = letterService.pickLetter();
         model.addAttribute("letter", letter);
         return "letter/letter";
+    }
+    @PostMapping("/letter")
+    @ResponseBody
+    public Letter letterLoad() {
+        Letter letter = letterService.pickLetter();
+        return letter;
     }
 
     @GetMapping("/letter/new")
